5ff3165703d1a2ae4130183d4efb9d15a68618ac
shahoriarniloy <niloyshahoriar@gmail.com>
2024-08-17 01:36:35 +0600

    Simplified

--- index.js
+++ index.js
@@ -28,7 +28,6 @@ app.use(
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dxgrzuk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -41,8 +40,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+   
 
     const database = client.db("techDB");
     const productsCollection =database.collection("products");
@@ -112,12 +110,9 @@ async function run() {
 
    
  
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+    
   }
 }
 run().catch(console.dir);
